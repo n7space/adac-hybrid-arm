a7ac2c93a72b813e3a8a016b836837ed7ee8de30
Michal Kurowski <mkurowski@n7space.com>
2022-03-04 17:39:15 +0100

    Automated Ads test execution

--- test/utils/execute_test.sh
+++ test/utils/execute_test.sh
@@ -2,14 +2,15 @@
 
 ELF_NAME="$(basename $1)"
 LOG_FILE="${ELF_NAME}.log"
+SCRIPT_DIR="$(dirname $0)"
 
 echo "Testing $1"
 
 echo "Starting QEMU with the binary"
-./simulate_on_qemu.sh $1 &
+${SCRIPT_DIR}/simulate_on_qemu.sh $1 &
 sleep 1
 echo "Executing GDB script logging breakpoint hits"
-arm-eabi-gdb -quiet -batch -x execute_test.gdb $1 > ${LOG_FILE}
+arm-eabi-gdb -quiet -batch -x ${SCRIPT_DIR}/execute_test.gdb $1 > ${LOG_FILE}
 echo "Killing QEMU"
 killall qemu-system-arm
 if [[ $(grep < ${LOG_FILE} Success ) = "\$1 = \"Success\"" ]]; then
